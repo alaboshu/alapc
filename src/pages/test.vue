c32f2d72800b1f8ead70d1d02dc09671f0b37832
zkcloud <asd@qq.com>
2019-10-10 08:31:48 +0800

    fix:username

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,5 +1,11 @@
 <template>
-  <img v-lazy='' />
+  <div class="user-card">
+    <img src='http://localhost:1800//wwwroot/static/images/GradeIcon/User03.png' alt='黄金会员' />
+    <div class="user-card-detail">
+      <a title='admin(老板) 等级:黄金会员' href='/Admin/User/Edit?id=1'>admin(老板)</a>
+      <span>黄金会员</span>
+    </div>
+  </div>
 </template>
 <script>
   export default {
@@ -19,7 +25,34 @@
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-  .icondemo {
-    font-size: 50px;
+  .user-card {
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    padding: 0;
+    margin: 0;
+    img {
+      width: 40px;
+      height: 40x;
+      margin: 0px;
+      margin-right: 0.5rem;
+    }
+    a {
+      display: block;
+      font-size: 1.1rem;
+      font-weight: bold;
+      line-height: 1.5rem;
+      color: #7b7e8a;
+    }
+    span {
+      margin-top: 3px;
+      display: inline-block;
+      font-size: 0.9rem;
+      line-height: 1rem;
+      color: #7b7e8a;
+      font-weight: 100;
+      background-blend-mode: color-dodge;
+    }
   }
 </style>
