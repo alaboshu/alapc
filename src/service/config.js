86d75c6df0c799ce4f8fb405edab7639a62af2f6
zkcloud <asd@qq.com>
2019-10-14 15:18:33 +0800

    fix: add config.js

---
+++ src/service/config.js
@@ -0,0 +1,17 @@
+// const apiBaseUrl = 'http://retail_v13.api.5ug.com'
+const apiBaseUrl = 'http://localhost:1800'
+
+const key =
+  'LUPGHODKYFWYO0YARAWNTKMA9WKWIO4ABXPYZAAFLKVIMWHKL2RAYBVAIPGKDGMJKV71L9BQA0LTZEYMOKVP6DMABP0K3PBF7M8QZQ8L1BA7ZMLEAE53V6NQS7ZWJ1WU'
+
+const projectId = 'C94E355F-79DC-44E8-8EF1-CBF886129EDA'
+const privateKey = ''
+const isTenant = true
+
+export default {
+  apiBaseUrl,
+  projectId,
+  key,
+  isTenant,
+  privateKey
+}
