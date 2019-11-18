1d785ecd4be6bb7d0610fa82d36f3cb23239ad61
sampsong <2839018@qq.com>
2019-11-18 22:57:39 +0800

    fix:x-single-table

--- src/components/reports/zk-single-report/items/01-number-item.vue
+++ src/components/reports/zk-single-report/items/01-number-item.vue
@@ -3,13 +3,10 @@
     <div v-for="(item,index) in viewModel" :key="index" :style="'width:'+100/count+'%;'+'display: inline-block;'">
       <div class="item" :style="{background:item.bgcolor}">
         <div class="item-box" :style="'color:' +item.color +';'">
-          <span class="item-count">{{item.count}}</span>
-          <span class="item-title">{{item.name}}</span>
+          <span class="item-count">{{getDate(item.count)}}</span>
           <i class="icon iconfont " :class="item.icon.name"></i>
         </div>
-        <router-link to="">
-          <p class="item-more">更多<i class="el-icon-arrow-right"></i></p>
-        </router-link>
+        <p class="item-more" :style="'color:' +item.color +';'">{{item.name}}</p>
       </div>
     </div>
   </div>
@@ -27,9 +24,64 @@
       count: {
         default: 4
       }
+    },
+    methods: {
+      getDate (data) {
+
+      }
     }
   }
 </script>
 <style rel="stylesheet/scss" lang="scss">
-  @import "./style.scss";
+  .number-item {
+    font-size: 12px;
+    .el-row {
+      box-sizing: border-box;
+      .el-col-md-6 {
+        width: 25%;
+      }
+    }
+    .item {
+      margin: 5px 10px;
+      box-sizing: border-box;
+      position: relative;
+      color: #fff;
+      border-radius: 3px;
+    }
+    .item-box {
+      padding: 10px 20px;
+      i {
+        transition: all 0.3s linear;
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        font-size: 65px;
+        color: rgba(0, 0, 0, 0.15);
+      }
+    }
+    .item-count {
+      margin-bottom: 5px;
+      font-size: 38px;
+      white-space: nowrap;
+      font-weight: 300;
+      display: block;
+      padding: 15px 0px;
+    }
+    .item-title {
+      font-size: 15px;
+    }
+    .item-more {
+      position: relative;
+      text-align: left;
+      padding: 3px 0;
+      color: #fff;
+      color: rgba(61, 32, 32, 0.8);
+      display: block;
+      z-index: 10;
+      font-size: 14px;
+      letter-spacing: 2px;
+      background: rgba(0, 0, 0, 0.1);
+      text-decoration: none;
+    }
+  }
 </style>
