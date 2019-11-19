dcd3803a0515d40a01ca1d641d6a7d4cc7cf02b6
Administrator <Administrator@DESKTOP-JD8QDF1>
2019-11-19 12:16:25 +0800

    fix:zk-report-table

--- src/components/reports/zk-report-table/styles/actions.js
+++ src/components/reports/zk-report-table/styles/actions.js
@@ -1,4 +1,55 @@
 export default {
+  // 获取报表数据
+  async getReport (jsThis, dataModel) {
+    var para = {
+      ...dataModel
+    }
+    var response = await jsThis.$api.httpPost(jsThis.reportApi, para)
+    if (response.status === 1) {
+      console.info('数据报表', response)
+      jsThis.chartData = response.result
+      jsThis.chartSettings.type = response.result.type
+    } else {
+      jsThis.$alert('配置出错，数据请求不成功，请重新配置')
+    }
+  },
+  // 获取表格数据
+  async getTable (jsThis, dataModel) {
+    var para = {
+      ...dataModel
+    }
+    para.pageIndex = 1
+    var tableresponse = await jsThis.$api.httpPost(jsThis.tableApi, para)
+    if (tableresponse.status === 1) {
+      console.info('表格统计', tableresponse.result)
+      for (let i in tableresponse.result.columns) {
+        if (tableresponse.result.columns[i] === '比率') {
+          tableresponse.result.columns.splice(0, 0, tableresponse.result.columns.splice(i, 1)[0])
+        }
+        if (tableresponse.result.columns[i] === '日期') {
+          tableresponse.result.columns.splice(1, 0, tableresponse.result.columns.splice(i, 1)[0])
+        }
+      }
+      jsThis.tableData = tableresponse.result
+    } else {
+      jsThis.$alert('配置出错，数据请求不成功，请重新配置')
+    }
+  },
+
+  // 分页功能
+  async pageChange (jsThis, ev) {
+    var para = {
+      ...jsThis.dataModel
+    }
+    jsThis.currentPage = ev
+    para.pageIndex = ev
+    var tableresponse = await jsThis.$api.httpPost('/api/Report/GetCountTable', para)
+    if (tableresponse.status === 1) {
+      jsThis.tableData = tableresponse.result
+    } else {
+      jsThis.$alert('配置出错，数据请求不成功，请重新配置')
+    }
+  },
   chartType (type) {
     switch (type) {
       case 0:
--- src/components/reports/zk-report-table/styles/count-report.vue
+++ src/components/reports/zk-report-table/styles/count-report.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="count-report">
-    <div class="">
+  <div class="count-report" v-if="async">
+    <div class="" v-if="dataModel.style !== 2">
       <ve-chart :grid="grid" :data="chartData" :extend="chartExtend" :colors="colors" :settings="chartSettings" v-if="dataModel.style!==2">
       </ve-chart>
     </div>
-    <div class="">两个区块链</div>
+    <div class="" v-if="dataModel.style !== 1">
+      <el-table :data="tableData.rows" style="width: 100%" :row-class-name="tableRowClassName">
+        <div class="table" v-for="(item, index) in tableData.columns" :key="index">
+          <el-table-column :prop="item" :label="item">
+          </el-table-column>
+        </div>
+      </el-table>
+      <div class="pagination-list">
+        <el-pagination @current-change="pagesChange" layout="prev, pager, next" :page-size="15" :current-page="currentPage" :total="tableData.totalCount">
+        </el-pagination>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +34,7 @@
         viewModel: '',
         chartData: {},
         chartExtend: {},
+        tableData: {},
         colors: ['#c23531', '#2f4554', '#61a0a8', '#d48265', '#91c7ae', '#749f83', '#ca8622', '#bda29a', '#6e7074', '#546570', '#c4ccd3'],
         grid: {
           show: true,
@@ -32,30 +44,52 @@
         },
         chartSettings: {
           type: 'line'
-        }
+        },
+        currentPage: 1,
+        reportApi: '/api/Report/GetCountReport',
+        tableApi: '/api/Report/GetCountTable',
+        async: false
       }
     },
     mounted () {
       this.init()
     },
     methods: {
-      async init () {
-        console.info('bbbbbbb', this.dataModel.reportType)
-        this.chartSettings.type = actions.chartType(this.dataModel.reportType)
-        var para = {
-          ...this.dataModel
-        }
+      init () {
         if (this.dataModel.style !== 2) {
-          var response = await this.$api.httpPost('/api/Report/GetCountReport', para)
-          if (response.status === 1) {
-            console.info('数据报表', response)
-            this.chartData = response.result
-            this.chartSettings.type = response.result.type
-          } else {
-            this.$alert('配置出错，数据请求不成功，请重新配置')
-          }
+          actions.getReport(this, this.dataModel)
         }
+        if (this.dataModel.style !== 1) {
+          actions.getTable(this, this.dataModel)
+        }
+        this.chartSettings.type = actions.chartType(this.dataModel.reportType)
+        this.async = true
+      },
+      // 设置颜色
+      tableRowClassName ({ row, rowIndex }) {
+        if ((rowIndex % 2) === 0) {
+          return ''
+        } else {
+          return 'success-row'
+        }
+      },
+      async pagesChange (ev) {
+        actions.pageChange(this, ev)
       }
     }
   }
 </script>
+<style lang="scss" scoped>
+  .el-table .warning-row {
+    background: oldlace;
+  }
+
+  .el-table .success-row {
+    background: #f0f9eb;
+  }
+  .pagination-list {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+</style>
