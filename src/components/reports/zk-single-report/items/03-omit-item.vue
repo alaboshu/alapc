65a298c119714cb50bde1afe04d985a85619f9d5
sampsong <2839018@qq.com>
2019-11-19 16:53:50 +0800

    fix：user service

---
+++ src/components/reports/zk-single-report/items/03-omit-item.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="omit-item">
+    <div class="omit-item_list" :style="'width:'+100/3+'%;'" v-for="(item,index) in viewModel" :key="index">
+      <div class="omit-item_list-box">
+        <div class="omit-item_left">
+          <div class="omit-item_title">{{item.name}}</div>
+          <div class="omit-item_intro">{{item.intro}}</div>
+        </div>
+        <div class="omit-item_right" :style="'color:'+item.fontColor+';'">{{getDate(item.value)}} {{viewNum}}</div>
+      </div>
+      <!-- <form-item :widget="item" type="omit-item" :widgetModel="viewModel"></form-item> -->
+    </div>
+  </div>
+</template>
+<script>
+  import minx from './minx/index'
+  export default {
+    mixins: [minx],
+    data () {
+      return {
+        widgetModel: '',
+        viewNum: null,
+        span: 6
+      }
+    },
+    props: {
+      viewModel: {},
+      count: {
+        default: 3
+      }
+    }
+  }
+</script>
+<style rel="stylesheet/scss" lang="scss">
+  .omit-item {
+    display: flex;
+    flex-wrap: wrap;
+    .omit-item_list {
+      border-right: 1px solid #f0f0f0;
+      padding: 0px 30px;
+      .omit-item_list-box {
+        display: flex;
+        align-items: center;
+        border-bottom: 1px dashed #f0f0f0;
+        padding: 15px 0px;
+        .omit-item_left {
+          flex: 1;
+          min-width: 0;
+          .omit-item_title {
+            font-size: 16px;
+            color: #333333;
+            font-weight: 500;
+          }
+          .omit-item_intro {
+            font-size: 12px;
+            color: #999999;
+            line-height: 20px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
+        }
+        .omit-item_right {
+          font-size: 18px;
+          font-weight: 600;
+        }
+      }
+    }
+  }
+</style>
