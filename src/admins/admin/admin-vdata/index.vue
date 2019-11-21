be8b14d6e4119030f1f1c50b2ee11552f15406de
Administrator <Administrator@DESKTOP-JD8QDF1>
2019-11-21 17:26:27 +0800

    feat:admin-vdata

--- src/admins/admin/admin-vdata/index.vue
+++ src/admins/admin/admin-vdata/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="admin-vdata">
-    <div class="admin-vdata-list"></div>
-  </div>
+  <x-border title="酷屏" type="brand">
+    <div class="admin-vdata">
+      <div class="admin-vdata-list" v-for="item in 10" :key="item">
+        <img class="admin-vdata-image" src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2934232835,3423836292&fm=26&gp=0.jpg" alt="" />
+        <div class="admin-vdata-cont">大数据屏幕</div>
+        <div class="admin-layout">
+          <div class="admin-layout-delete" title="删除模板">删除模板</div>
+          <div class="admin-layout-cont">
+            <div class="admin-layout-button left-background">编辑</div>
+            <div class="admin-layout-button right-background">预览</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </x-border>
 </template>
 
 <script>
