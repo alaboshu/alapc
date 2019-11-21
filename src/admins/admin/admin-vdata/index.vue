8287780b9ad6498983fec415820fb0a05a4d6ac7
Administrator <Administrator@DESKTOP-JD8QDF1>
2019-11-21 16:45:34 +0800

    fix:admin

--- src/admins/admin/admin-vdata/index.vue
+++ src/admins/admin/admin-vdata/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>酷屏</div>
+  <div class="admin-vdata">
+    <div class="admin-vdata-list"></div>
+  </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        adminVData: {
+
+        }
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  @import "./var.scss";
+</style>
