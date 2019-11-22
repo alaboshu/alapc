f00ea164d053016243c7a7ce562d5558e6e139b6
sampsong <2839018@qq.com>
2019-11-22 16:37:05 +0800

    fix:v-data

--- src/admins/admin/admin-vdata/index.vue
+++ src/admins/admin/admin-vdata/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <x-border title="酷屏" type="brand">
-    <div class="admin-vdata">
-      <div class="admin-vdata-list" v-for="item in 10" :key="item">
+  <x-border title="酷屏" type="focus" icon="icon-5333-sudu">
+    <div class="admin-vdata" v-if="viewModel">
+      <div class="admin-vdata-list" v-for="item in viewModel" :key="item">
         <img class="admin-vdata-image" src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2934232835,3423836292&fm=26&gp=0.jpg" alt="" />
-        <div class="admin-vdata-cont">大数据屏幕</div>
+        <div class="admin-vdata-cont">{{item.name}}</div>
         <div class="admin-layout">
           <div class="admin-layout-delete" title="删除模板">删除模板</div>
           <div class="admin-layout-cont">
@@ -20,7 +20,8 @@
   export default {
     data () {
       return {
-        adminVData: {}
+        adminVData: {},
+        viewModel: null
       }
     },
     mounted () {
@@ -29,7 +30,10 @@
     methods: {
       async init () {
         var response = await this.$api.httpGet('/api/themepage/querylist?isVDataPage=true')
-        console.info('response', response)
+        if (response.status === 1) {
+          this.viewModel = response.result
+        }
+        console.info('response', this.viewModel)
       }
     }
   }
