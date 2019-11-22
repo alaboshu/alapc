5078536073a0454f4edb59b43a5f503be3aab431
sampsong <2839018@qq.com>
2019-11-22 16:41:39 +0800

    fix:index.vue

--- src/admins/admin/admin-vdata/index.vue
+++ src/admins/admin/admin-vdata/index.vue
@@ -5,10 +5,10 @@
         <img class="admin-vdata-image" src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2934232835,3423836292&fm=26&gp=0.jpg" alt="" />
         <div class="admin-vdata-cont">{{item.name}}</div>
         <div class="admin-layout">
-          <div class="admin-layout-delete" title="删除模板">删除模板</div>
+          <div class="admin-layout-delete" title="删除模板" v-if="false">删除模板</div>
           <div class="admin-layout-cont">
-            <div class="admin-layout-button left-background">编辑</div>
-            <div class="admin-layout-button right-background">预览</div>
+            <div class="admin-layout-button left-background" :href="'/admin/vdata/preview?path='+item.path" target="_blank">编辑</div>
+            <a class="admin-layout-button right-background" :href="'/admin/vdata/preview?path='+item.path" target="_blank">预览</a>
           </div>
         </div>
       </div>
