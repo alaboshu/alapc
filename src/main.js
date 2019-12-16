27dbce595145a08f00c1219f7e87fbd255ecd84f
sampsong <2839018@qq.com>
2019-12-16 16:11:37 +0800

    fix: theme componet

--- src/main.js
+++ src/main.js
@@ -20,76 +20,22 @@ import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 import VueDraggableResizable from 'vue-draggable-resizable'
 import zkGroupbuy from '@/components/activitys/zk-groupbuy/index.vue'
 import zkAritcleDetail from '@/components/articles/zk-aritcle-detail/index.vue'
-import zkAriticleImageList from '@/components/articles/zk-ariticle-image-list/index.vue'
 import zkArticle from '@/components/articles/zk-article/index.vue'
-import zkHelpRow from '@/components/articles/zk-help-row/index.vue'
 import zkHelp from '@/components/articles/zk-help/index.vue'
-import zkMarquee from '@/components/articles/zk-marquee/index.vue'
-import zkNewsItem from '@/components/articles/zk-news-item/index.vue'
-import zkNewsList from '@/components/articles/zk-news-list/index.vue'
-import zkNewsNav from '@/components/articles/zk-news-nav/index.vue'
-import zkNewsProduct from '@/components/articles/zk-news-product/index.vue'
-import zkNewsRec from '@/components/articles/zk-news-rec/index.vue'
-import zkNewsShow from '@/components/articles/zk-news-show/index.vue'
 import zkBacktop from '@/components/common/zk-backtop/index.vue'
-import zkClear from '@/components/common/zk-clear/index.vue'
 import zkLogo from '@/components/common/zk-logo/index.vue'
 import zkNav from '@/components/common/zk-nav/index.vue'
 import zkPay from '@/components/common/zk-pay/index.vue'
 import zkProgress from '@/components/common/zk-progress/index.vue'
 import zkTopNav from '@/components/common/zk-top-nav/index.vue'
-import zkDataHiding from '@/components/data/zk-data-hiding/index.vue'
-import zkDataInfo from '@/components/data/zk-data-info/index.vue'
-import zkDataList from '@/components/data/zk-data-list/index.vue'
-import zkDataMember from '@/components/data/zk-data-member/index.vue'
-import zkDataOption from '@/components/data/zk-data-option/index.vue'
-import zkDataPackages from '@/components/data/zk-data-packages/index.vue'
-import zkDataSmall from '@/components/data/zk-data-small/index.vue'
 import zkAmount from '@/components/finance/zk-amount/index.vue'
 import zkBuy from '@/components/order/zk-buy/index.vue'
 import zkCart from '@/components/order/zk-cart/index.vue'
 import zkOrderList from '@/components/order/zk-order-list/index.vue'
 import zkOrderRate from '@/components/order/zk-order-rate/index.vue'
 import zkOrderShow from '@/components/order/zk-order-show/index.vue'
-import zkPreferList from '@/components/product/zk-prefer-list/index.vue'
-import zkProductAdvertisement from '@/components/product/zk-product-advertisement/index.vue'
-import zkProductArrival from '@/components/product/zk-product-arrival/index.vue'
-import zkProductBrand from '@/components/product/zk-product-brand/index.vue'
-import zkProductBrandhead from '@/components/product/zk-product-brandhead/index.vue'
-import zkProductBrandlist from '@/components/product/zk-product-brandlist/index.vue'
 import zkProductClass from '@/components/product/zk-product-class/index.vue'
-import zkProductCustomerService from '@/components/product/zk-product-customer-service/index.vue'
-import zkProductData from '@/components/product/zk-product-data/index.vue'
-import zkProductEspeciallyhead from '@/components/product/zk-product-especiallyhead/index.vue'
-import zkProductFavorite from '@/components/product/zk-product-favorite/index.vue'
-import zkProductFlashsale from '@/components/product/zk-product-flashsale/index.vue'
-import zkProductFoot from '@/components/product/zk-product-foot/index.vue'
-import zkProductGoods from '@/components/product/zk-product-goods/index.vue'
-import zkProductHead from '@/components/product/zk-product-head/index.vue'
-import zkProductHeadline from '@/components/product/zk-product-headline/index.vue'
-import zkProductImagelist from '@/components/product/zk-product-imagelist/index.vue'
-import zkProductItem from '@/components/product/zk-product-item/index.vue'
-import zkProductLinks from '@/components/product/zk-product-links/index.vue'
 import zkProductList from '@/components/product/zk-product-list/index.vue'
-import zkProductListitem from '@/components/product/zk-product-listitem/index.vue'
-import zkProductListpage from '@/components/product/zk-product-listpage/index.vue'
-import zkProductLogin from '@/components/product/zk-product-login/index.vue'
-import zkProductMustsee from '@/components/product/zk-product-mustsee/index.vue'
-import zkProductNav from '@/components/product/zk-product-nav/index.vue'
-import zkProductRecommend from '@/components/product/zk-product-recommend/index.vue'
-import zkProductReg from '@/components/product/zk-product-reg/index.vue'
-import zkProductScreening from '@/components/product/zk-product-screening/index.vue'
-import zkProductSelecthead from '@/components/product/zk-product-selecthead/index.vue'
-import zkProductSelection from '@/components/product/zk-product-selection/index.vue'
-import zkProductShowpage from '@/components/product/zk-product-showpage/index.vue'
-import zkProductShuffling from '@/components/product/zk-product-shuffling/index.vue'
-import zkProductStorecat from '@/components/product/zk-product-storecat/index.vue'
-import zkProductSuperior from '@/components/product/zk-product-superior/index.vue'
-import zkProductSwiper from '@/components/product/zk-product-swiper/index.vue'
-import zkProductSwiperitem from '@/components/product/zk-product-swiperitem/index.vue'
-import zkProductTop from '@/components/product/zk-product-top/index.vue'
-import zkProductTypeNav from '@/components/product/zk-product-type-nav/index.vue'
-import zkProductclassItem from '@/components/product/zk-productclass-item/index.vue'
 import zkReportTable from '@/components/reports/zk-report-table/index.vue'
 import zkSingleReport from '@/components/reports/zk-single-report/index.vue'
 import zkAddress from '@/components/themes/zk-address/index.vue'
@@ -97,60 +43,31 @@ import zkAudio from '@/components/themes/zk-audio/index.vue'
 import zkAutoDialog from '@/components/themes/zk-auto-dialog/index.vue'
 import zkAutoForm from '@/components/themes/zk-auto-form/index.vue'
 import zkAvatar from '@/components/themes/zk-avatar/index.vue'
-import zkBuyAddress from '@/components/themes/zk-buy-address/index.vue'
-import zkCard from '@/components/themes/zk-card/index.vue'
-import zkCell from '@/components/themes/zk-cell/index.vue'
 import zkCountdown from '@/components/themes/zk-countdown/index.vue'
 import zkDialog from '@/components/themes/zk-dialog/index.vue'
 import zkFileManage from '@/components/themes/zk-file-manage/index.vue'
 import zkFoot from '@/components/themes/zk-foot/index.vue'
-import zkGridList from '@/components/themes/zk-grid-list/index.vue'
 import zkGrid from '@/components/themes/zk-grid/index.vue'
 import zkHead from '@/components/themes/zk-head/index.vue'
 import zkHtml from '@/components/themes/zk-html/index.vue'
-import zkIconAd from '@/components/themes/zk-icon-ad/index.vue'
-import zkIconList from '@/components/themes/zk-icon-list/index.vue'
 import zkImageList from '@/components/themes/zk-image-list/index.vue'
 import zkImage from '@/components/themes/zk-image/index.vue'
 import zkKeyword from '@/components/themes/zk-keyword/index.vue'
 import zkList from '@/components/themes/zk-list/index.vue'
-import zkManagement from '@/components/themes/zk-management/index.vue'
-import zkMask from '@/components/themes/zk-mask/index.vue'
-import zkNodata from '@/components/themes/zk-nodata/index.vue'
-import zkNotice from '@/components/themes/zk-notice/index.vue'
 import zkPopup from '@/components/themes/zk-popup/index.vue'
-import zkPreview from '@/components/themes/zk-preview/index.vue'
-import zkQrcodeList from '@/components/themes/zk-qrcode-list/index.vue'
 import zkQuickAction from '@/components/themes/zk-quick-action/index.vue'
-import zkRankReport from '@/components/themes/zk-rank-report/index.vue'
-import zkReports from '@/components/themes/zk-reports/index.vue'
-import zkResult from '@/components/themes/zk-result/index.vue'
-import zkRightbar from '@/components/themes/zk-rightbar/index.vue'
 import zkSearch from '@/components/themes/zk-search/index.vue'
 import zkSwiperNav from '@/components/themes/zk-swiper-nav/index.vue'
 import zkSwiper from '@/components/themes/zk-swiper/index.vue'
-import zkTab from '@/components/themes/zk-tab/index.vue'
-import zkTableList from '@/components/themes/zk-table-list/index.vue'
-import zkTableVideoList from '@/components/themes/zk-table-video-list/index.vue'
 import zkTable from '@/components/themes/zk-table/index.vue'
-import zkTempRegion from '@/components/themes/zk-tempRegion/index.vue'
 import zkText from '@/components/themes/zk-text/index.vue'
 import zkTitle from '@/components/themes/zk-title/index.vue'
 import zkTop from '@/components/themes/zk-top/index.vue'
-import zkTreetable from '@/components/themes/zk-treetable/index.vue'
 import zkVideo from '@/components/themes/zk-video/index.vue'
 import zkAddressEdit from '@/components/user/zk-address-edit/index.vue'
 import zkHeadUser from '@/components/user/zk-head-user/index.vue'
-import zkLoginMobile from '@/components/user/zk-login-mobile/index.vue'
-import zkPclogin from '@/components/user/zk-pclogin/index.vue'
-import zkPcloginout from '@/components/user/zk-pcloginout/index.vue'
-import zkPcreg from '@/components/user/zk-pcreg/index.vue'
 import zkQrcode from '@/components/user/zk-qrcode/index.vue'
 import zkUserAddress from '@/components/user/zk-user-address/index.vue'
-import zkUserBase from '@/components/user/zk-user-base/index.vue'
-import zkUserNav from '@/components/user/zk-user-nav/index.vue'
-import zkUserView from '@/components/user/zk-user-view/index.vue'
-import zkUsersIndex from '@/components/user/zk-users-index/index.vue'
 import xAutoconfig from '@/elements/x-autoconfig/index.vue'
 import xBadge from '@/elements/x-badge/index.vue'
 import xBorder from '@/elements/x-border/index.vue'
@@ -248,76 +165,22 @@ import adminRoleOpen from '@/admins/user/admin-role-open/index.vue'
 import adminUserEdit from '@/admins/user/admin-user-edit/index.vue'
 Vue.component('zk-groupbuy', zkGroupbuy)
 Vue.component('zk-aritcle-detail', zkAritcleDetail)
-Vue.component('zk-ariticle-image-list', zkAriticleImageList)
 Vue.component('zk-article', zkArticle)
-Vue.component('zk-help-row', zkHelpRow)
 Vue.component('zk-help', zkHelp)
-Vue.component('zk-marquee', zkMarquee)
-Vue.component('zk-news-item', zkNewsItem)
-Vue.component('zk-news-list', zkNewsList)
-Vue.component('zk-news-nav', zkNewsNav)
-Vue.component('zk-news-product', zkNewsProduct)
-Vue.component('zk-news-rec', zkNewsRec)
-Vue.component('zk-news-show', zkNewsShow)
 Vue.component('zk-backtop', zkBacktop)
-Vue.component('zk-clear', zkClear)
 Vue.component('zk-logo', zkLogo)
 Vue.component('zk-nav', zkNav)
 Vue.component('zk-pay', zkPay)
 Vue.component('zk-progress', zkProgress)
 Vue.component('zk-top-nav', zkTopNav)
-Vue.component('zk-data-hiding', zkDataHiding)
-Vue.component('zk-data-info', zkDataInfo)
-Vue.component('zk-data-list', zkDataList)
-Vue.component('zk-data-member', zkDataMember)
-Vue.component('zk-data-option', zkDataOption)
-Vue.component('zk-data-packages', zkDataPackages)
-Vue.component('zk-data-small', zkDataSmall)
 Vue.component('zk-amount', zkAmount)
 Vue.component('zk-buy', zkBuy)
 Vue.component('zk-cart', zkCart)
 Vue.component('zk-order-list', zkOrderList)
 Vue.component('zk-order-rate', zkOrderRate)
 Vue.component('zk-order-show', zkOrderShow)
-Vue.component('zk-prefer-list', zkPreferList)
-Vue.component('zk-product-advertisement', zkProductAdvertisement)
-Vue.component('zk-product-arrival', zkProductArrival)
-Vue.component('zk-product-brand', zkProductBrand)
-Vue.component('zk-product-brandhead', zkProductBrandhead)
-Vue.component('zk-product-brandlist', zkProductBrandlist)
 Vue.component('zk-product-class', zkProductClass)
-Vue.component('zk-product-customer-service', zkProductCustomerService)
-Vue.component('zk-product-data', zkProductData)
-Vue.component('zk-product-especiallyhead', zkProductEspeciallyhead)
-Vue.component('zk-product-favorite', zkProductFavorite)
-Vue.component('zk-product-flashsale', zkProductFlashsale)
-Vue.component('zk-product-foot', zkProductFoot)
-Vue.component('zk-product-goods', zkProductGoods)
-Vue.component('zk-product-head', zkProductHead)
-Vue.component('zk-product-headline', zkProductHeadline)
-Vue.component('zk-product-imagelist', zkProductImagelist)
-Vue.component('zk-product-item', zkProductItem)
-Vue.component('zk-product-links', zkProductLinks)
 Vue.component('zk-product-list', zkProductList)
-Vue.component('zk-product-listitem', zkProductListitem)
-Vue.component('zk-product-listpage', zkProductListpage)
-Vue.component('zk-product-login', zkProductLogin)
-Vue.component('zk-product-mustsee', zkProductMustsee)
-Vue.component('zk-product-nav', zkProductNav)
-Vue.component('zk-product-recommend', zkProductRecommend)
-Vue.component('zk-product-reg', zkProductReg)
-Vue.component('zk-product-screening', zkProductScreening)
-Vue.component('zk-product-selecthead', zkProductSelecthead)
-Vue.component('zk-product-selection', zkProductSelection)
-Vue.component('zk-product-showpage', zkProductShowpage)
-Vue.component('zk-product-shuffling', zkProductShuffling)
-Vue.component('zk-product-storecat', zkProductStorecat)
-Vue.component('zk-product-superior', zkProductSuperior)
-Vue.component('zk-product-swiper', zkProductSwiper)
-Vue.component('zk-product-swiperitem', zkProductSwiperitem)
-Vue.component('zk-product-top', zkProductTop)
-Vue.component('zk-product-type-nav', zkProductTypeNav)
-Vue.component('zk-productclass-item', zkProductclassItem)
 Vue.component('zk-report-table', zkReportTable)
 Vue.component('zk-single-report', zkSingleReport)
 Vue.component('zk-address', zkAddress)
@@ -325,60 +188,31 @@ Vue.component('zk-audio', zkAudio)
 Vue.component('zk-auto-dialog', zkAutoDialog)
 Vue.component('zk-auto-form', zkAutoForm)
 Vue.component('zk-avatar', zkAvatar)
-Vue.component('zk-buy-address', zkBuyAddress)
-Vue.component('zk-card', zkCard)
-Vue.component('zk-cell', zkCell)
 Vue.component('zk-countdown', zkCountdown)
 Vue.component('zk-dialog', zkDialog)
 Vue.component('zk-file-manage', zkFileManage)
 Vue.component('zk-foot', zkFoot)
-Vue.component('zk-grid-list', zkGridList)
 Vue.component('zk-grid', zkGrid)
 Vue.component('zk-head', zkHead)
 Vue.component('zk-html', zkHtml)
-Vue.component('zk-icon-ad', zkIconAd)
-Vue.component('zk-icon-list', zkIconList)
 Vue.component('zk-image-list', zkImageList)
 Vue.component('zk-image', zkImage)
 Vue.component('zk-keyword', zkKeyword)
 Vue.component('zk-list', zkList)
-Vue.component('zk-management', zkManagement)
-Vue.component('zk-mask', zkMask)
-Vue.component('zk-nodata', zkNodata)
-Vue.component('zk-notice', zkNotice)
 Vue.component('zk-popup', zkPopup)
-Vue.component('zk-preview', zkPreview)
-Vue.component('zk-qrcode-list', zkQrcodeList)
 Vue.component('zk-quick-action', zkQuickAction)
-Vue.component('zk-rank-report', zkRankReport)
-Vue.component('zk-reports', zkReports)
-Vue.component('zk-result', zkResult)
-Vue.component('zk-rightbar', zkRightbar)
 Vue.component('zk-search', zkSearch)
 Vue.component('zk-swiper-nav', zkSwiperNav)
 Vue.component('zk-swiper', zkSwiper)
-Vue.component('zk-tab', zkTab)
-Vue.component('zk-table-list', zkTableList)
-Vue.component('zk-table-video-list', zkTableVideoList)
 Vue.component('zk-table', zkTable)
-Vue.component('zk-tempRegion', zkTempRegion)
 Vue.component('zk-text', zkText)
 Vue.component('zk-title', zkTitle)
 Vue.component('zk-top', zkTop)
-Vue.component('zk-treetable', zkTreetable)
 Vue.component('zk-video', zkVideo)
 Vue.component('zk-address-edit', zkAddressEdit)
 Vue.component('zk-head-user', zkHeadUser)
-Vue.component('zk-login-mobile', zkLoginMobile)
-Vue.component('zk-pclogin', zkPclogin)
-Vue.component('zk-pcloginout', zkPcloginout)
-Vue.component('zk-pcreg', zkPcreg)
 Vue.component('zk-qrcode', zkQrcode)
 Vue.component('zk-user-address', zkUserAddress)
-Vue.component('zk-user-base', zkUserBase)
-Vue.component('zk-user-nav', zkUserNav)
-Vue.component('zk-user-view', zkUserView)
-Vue.component('zk-users-index', zkUsersIndex)
 Vue.component('x-autoconfig', xAutoconfig)
 Vue.component('x-badge', xBadge)
 Vue.component('x-border', xBorder)
