2816f2dfa13244f423478083e7ff453280e9734f
sampsong <2839018@qq.com>
2019-12-17 23:25:08 +0800

    fix:theme

--- src/admins/diy/admin-diy-theme/style/item.vue
+++ src/admins/diy/admin-diy-theme/style/item.vue
@@ -37,7 +37,9 @@
         var para = {
           themeId: theme.id
         }
+        this.$api.progressOpen('正在登录中,预计时间<span style="color: red;">1分钟</span>，请勿离开或刷新页面...')
         var response = await this.$api.httpGet('/api/theme/GetLoginUrl', para)
+        this.$api.progressClose()
         if (response.status === 1) {
           window.open(response.message, '_blank')
         }
